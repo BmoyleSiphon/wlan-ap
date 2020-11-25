47f406cede0a2f3e03e452801223df2facb5bf74
Luka Kudra <luka.kudra@netexperience.com>
2020-11-25 15:26:53 +0100

    add ubus object for dhcp events
    Signed-off-by: Luka Kudra <luka.kudra@netexperience.com>

--- feeds/wlan-ap/opensync/src/platform/openwrt/src/netifd/src/ubus.c
+++ feeds/wlan-ap/opensync/src/platform/openwrt/src/netifd/src/ubus.c
@@ -9,11 +9,6 @@
 
 static struct ubus_context *ubus;
 
-static void netifd_ubus_connect(struct ubus_context *ctx)
-{
-	ubus = ctx;
-}
-
 static int netifd_ubus_notify(struct ubus_context *ctx, struct ubus_object *obj,
 			     struct ubus_request_data *req, const char *method,
 			     struct blob_attr *msg)
@@ -23,6 +18,8 @@ static int netifd_ubus_notify(struct ubus_context *ctx, struct ubus_object *obj,
 	str = blobmsg_format_json(msg, true);
 	LOGN("ubus: Received ubus notify '%s': %s\n", method, str);
 	free(str);
+
+	LOG(INFO, "lkudra, method: %s", method);
 
 	if (!strncmp(method, "dhcp.", 5)) {
 		dhcp_lease(method, msg);
@@ -34,6 +31,88 @@ static int netifd_ubus_notify(struct ubus_context *ctx, struct ubus_object *obj,
 	return 0;
 }
 
+static int
+ubus_dhcp_ack_events_cb(struct ubus_context *ctx, struct ubus_object *obj,
+		    struct ubus_request_data *req, const char *method,
+		    struct blob_attr *msg)
+{
+	return UBUS_STATUS_OK;
+}
+
+static int
+ubus_dhcp_nak_events_cb(struct ubus_context *ctx, struct ubus_object *obj,
+		    struct ubus_request_data *req, const char *method,
+		    struct blob_attr *msg)
+{
+	return UBUS_STATUS_OK;
+}
+
+static int
+ubus_dhcp_offer_events_cb(struct ubus_context *ctx, struct ubus_object *obj,
+		    struct ubus_request_data *req, const char *method,
+		    struct blob_attr *msg)
+{
+	return UBUS_STATUS_OK;
+}
+
+static int
+ubus_dhcp_inform_events_cb(struct ubus_context *ctx, struct ubus_object *obj,
+		    struct ubus_request_data *req, const char *method,
+		    struct blob_attr *msg)
+{
+	return UBUS_STATUS_OK;
+}
+
+static int
+ubus_dhcp_decline_events_cb(struct ubus_context *ctx, struct ubus_object *obj,
+		    struct ubus_request_data *req, const char *method,
+		    struct blob_attr *msg)
+{
+	return UBUS_STATUS_OK;
+}
+
+static int
+ubus_dhcp_request_events_cb(struct ubus_context *ctx, struct ubus_object *obj,
+		    struct ubus_request_data *req, const char *method,
+		    struct blob_attr *msg)
+{
+	return UBUS_STATUS_OK;
+}
+
+static int
+ubus_dhcp_discover_events_cb(struct ubus_context *ctx, struct ubus_object *obj,
+		    struct ubus_request_data *req, const char *method,
+		    struct blob_attr *msg)
+{
+	return UBUS_STATUS_OK;
+}
+
+static const struct ubus_method netifd_ubus_methods[] = {
+        UBUS_METHOD_NOARG("get_dhcp_ack_events", ubus_dhcp_ack_events_cb),
+        UBUS_METHOD_NOARG("get_dhcp_nak_events", ubus_dhcp_nak_events_cb),
+        UBUS_METHOD_NOARG("get_dhcp_offer_events", ubus_dhcp_offer_events_cb),
+        UBUS_METHOD_NOARG("get_dhcp_inform_events", ubus_dhcp_inform_events_cb),
+        UBUS_METHOD_NOARG("get_dhcp_decline_events", ubus_dhcp_decline_events_cb),
+        UBUS_METHOD_NOARG("get_dhcp_request_events", ubus_dhcp_request_events_cb),
+        UBUS_METHOD_NOARG("get_dhcp_discover_events", ubus_dhcp_discover_events_cb),
+};
+
+static struct ubus_object_type netifd_ubus_object_type =
+        UBUS_OBJECT_TYPE("osync-dhcp", netifd_ubus_methods);
+
+static struct ubus_object netifd_ubus_object = {
+        .name = "osync-dhcp",
+        .type = &netifd_ubus_object_type,
+        .methods = netifd_ubus_methods,
+        .n_methods = ARRAY_SIZE(netifd_ubus_methods),
+};
+
+static void netifd_ubus_connect(struct ubus_context *ctx)
+{
+	ubus = ctx;
+	ubus_add_object(ubus, &netifd_ubus_object);
+}
+
 static struct ubus_instance ubus_instance = {
 	.connect = netifd_ubus_connect,
 	.notify = netifd_ubus_notify,
@@ -41,7 +120,8 @@ static struct ubus_instance ubus_instance = {
 			{
 				.path = "network.interface.",
 				.wildcard = 1,
-			}, {
+			},
+			{
 				.path = "dnsmasq",
 			},
 		},
